07def2913a4c2fd0b1c527fbcfbb9129b52950a1
Sergii Gorbatyi <sergii@vidigami.com>
2021-03-17 10:35:30 -0700

    fizzBuzz propblem

---
+++ fizz_buzz.js
@@ -0,0 +1,19 @@
+const n = 1;
+
+const fizzBuzz = function (n) {
+  let array = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      array.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      array.push("Fizz");
+    } else if (i % 5 === 0) {
+      array.push("Buzz");
+    } else {
+      array.push(`${i}`);
+    }
+  }
+  return array;
+};
+
+console.log(fizzBuzz(n));
